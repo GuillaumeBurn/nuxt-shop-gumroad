d2939e0e522f63ded1b313daeb94dc2e6f717f12
Indrasinh Bihola <biholaindrasinh@gmail.com>
2020-06-13 20:09:24 +0530

    Issue fixed.

--- middleware/products.js
+++ middleware/products.js
@@ -1,9 +1,13 @@
 export default async function({ store, $axios, params }) {
   // All products
+  $axios.setHeader(
+    "AUTHORIZATION",
+    "Bearer 4c3213a4bf4b963dcbd02fda2962d5b4fa804133b76f4853c2964067bac23dca"
+  );
   const productsObj = await $axios.$get("api/products");
   console.log(productsObj);
   store.dispatch("setProducts", productsObj.products);
   //Single product
-  const productObj = await $axios.$get(`api/products/${params.id}`);
-  store.dispatch("setProduct", productObj.product);
+  // const productObj = await $axios.$get(`api/products/${params.id}`);
+  // store.dispatch("setProduct", productObj.product);
 }
