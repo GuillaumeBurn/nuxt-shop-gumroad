5d0b0f71168cf14ad5123b25bf36786c01e0bd7b
guillaumeb <guillaumeb@guillaumebs-MacBook-Pro.local>
2020-06-14 11:15:26 -0400

    id page was missing

--- middleware/products.js
+++ middleware/products.js
@@ -1,4 +1,8 @@
 export default async function({ store, $axios, params }) {
+  $axios.setHeader(
+    "AUTHORIZATION",
+    "Bearer 4c3213a4bf4b963dcbd02fda2962d5b4fa804133b76f4853c2964067bac23dca"
+  );
   // All products
   const productsObj = await $axios.$get("api/products");
   console.log(productsObj);
