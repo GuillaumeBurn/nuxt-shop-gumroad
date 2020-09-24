6059467110fd2e9c8534060b25928092c5cee198
guillaumeb <guillaume.bernier@klarr.ca>
2020-09-24 17:24:27 -0400

    layout ajustements

---
+++ pages/shop/icons/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pageWrapper">
+    <ProductGrid :data="icons" />
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+import ProductGrid from "@/layouts/partials/ProductGrid";
+export default {
+  components: {
+    ProductGrid
+  },
+
+  computed: {
+    ...mapGetters(["icons"])
+  }
+};
+</script>
---
+++ pages/shop/mockups/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pageWrapper">
+    <ProductGrid :data="mockups" />
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+import ProductGrid from "@/layouts/partials/ProductGrid";
+export default {
+  components: {
+    ProductGrid
+  },
+
+  computed: {
+    ...mapGetters(["mockups"])
+  }
+};
+</script>
